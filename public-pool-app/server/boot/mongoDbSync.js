01b49d9bdcb6b611d11f92440a9ed2b7fd5d627e
Gianmario <s262472@studenti.polito.it>
2021-04-10 12:25:25 +0200

    developed public-pool-app, connection and autoupdate with a mongo datasource

---
+++ public-pool-app/server/boot/mongoDbSync.js
@@ -0,0 +1,55 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+
+const extractDataSourceModels = (modelConfigPath, dataSourceName, callback) => {
+  console.log('Extracting models from config file:');
+  fs.readFile(modelConfigPath, (err, data) => {
+    if (err) return callback(err);
+    const models = JSON.parse(data.toString('utf8'));
+    const modelKeys = Object.keys(models);
+    const modelKeysL = modelKeys.length;
+    const datasourceModels = [];
+    for (let i = 0; i < modelKeysL; i++) {
+      const currKey = modelKeys[i];
+      if (currKey !== '_meta') {
+        const model = models[currKey];
+        const currDatasource = model.dataSource;
+        if (currDatasource === dataSourceName) datasourceModels.push(currKey);
+      }
+    }
+    return callback(null, datasourceModels);
+  });
+};
+
+const autoSyncDatasource = (dataSource, callback) => {
+  console.log('Syncing database: ');
+  const dataSourceName = dataSource.name;
+  // eslint-disable-next-line max-len
+  const modelConfigPath = path.join(process.cwd(), 'server', 'model-config.json');
+
+  // extract the model's array
+  // eslint-disable-next-line max-len
+  extractDataSourceModels(modelConfigPath, dataSourceName, (err, datasourceModels) => {
+    if (err) return console.error(err);
+    console.log(datasourceModels);
+    dataSource.isActual(datasourceModels, (err, actual) => {
+      if (err) return console.err(err);
+      if (actual) {
+        console.log('Remote source is actual, no need to sync models');
+        return callback();
+      }
+      console.log('Remote source is not actual, syncing');
+      dataSource.autoupdate(datasourceModels, (err, result) => {
+        if (err) return console.log('Error: ', err);
+        console.log('DB synced successfully');
+        return callback();
+      });
+    });
+  });
+};
+
+module.exports = function(app, cb) {
+  const dataSourceMysql = app.datasources.mongoDs;
+  autoSyncDatasource(dataSourceMysql, cb);
+};
