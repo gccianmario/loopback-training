886072687f34b6cff39cb449483173ef404197a0
Gianmario <s262472@studenti.polito.it>
2021-05-01 00:27:01 +0200

    added the op hook for deletion of client and supplier

--- plant-seller-app/common/models/client.js
+++ plant-seller-app/common/models/client.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function(Client) {
+  const app = require("../../server/server");
 
+  Client.observe("before delete", async (ctx) => {
+    console.log("id of client.. "+ ctx.where.id)
+    const purchaseList = await app.models.Purchase.find(
+      {
+        where: {client_id: ctx.where.id}
+      }
+    )
+    if (await purchaseList.length !== 0)
+      throw new Error(`client has ${await purchaseList.length} purchase associated=>cannot be deleted`)
+  })
 };
--- plant-seller-app/common/models/supplier.js
+++ plant-seller-app/common/models/supplier.js
@@ -1,5 +1,34 @@
 'use strict';
 
 module.exports = function(Supplier) {
+  const app = require("../../server/server");
 
+  const verifyIfSupplierCanBeDeleted = (supplier_id) =>{
+    return new Promise(resolve => {
+      //find species supplied by this supplier if any
+      app.models.Specie.find({
+        where:{
+          supplier_id: supplier_id
+        }
+      }).then((suppliedSpecies)=>{
+        if(suppliedSpecies.length === 0)
+          resolve(true)
+        const specieNames = []
+        suppliedSpecies.forEach(s=>specieNames.push(s.id))
+        //search if any purchase involves any species supplied by this supplier
+        app.models.Purchase.find({
+          where:{
+            specie_id : { inq: specieNames}
+          }
+        }).then((listOfAllPurchases)=>{
+          resolve(listOfAllPurchases.length === 0)
+        }).catch(err=>console.log("purchase find error cannot delete " + err))
+      }).catch(err=>console.log("specie find error cannot delete " + err))
+    })
+  }
+
+  Supplier.observe("before delete", async (ctx) => {
+    const res = await verifyIfSupplierCanBeDeleted(ctx.where.ssn)
+    if (!res) throw new Error("supplier cannot be deleted, it is involved in transactions");
+  })
 };
