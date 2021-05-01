1d4e5cd62409915842420f195e3d831bbb5b7a0a
Gianmario <s262472@studenti.polito.it>
2021-05-01 22:12:47 +0200

    added remote method for pricehistory with specie attached

--- plant-seller-app/common/models/price-history.js
+++ plant-seller-app/common/models/price-history.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(Pricehistory) {
+  const app = require("../../server/server");
 
+  Pricehistory.withSpecies = async function() {
+    return app.models.PriceHistory.find({
+      include: {
+        relation: "specie"
+      }
+    })
+  }
+
+  Pricehistory.remoteMethod('withSpecies', {
+    returns: {arg: 'data', type: 'object'},
+  });
 };
